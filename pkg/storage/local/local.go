f073cce05a2eefeb0eb7aa05ba69358e8c9a05b4
qiaofufu <707402933@qq.com>
2023-09-08 17:08:09 +0800

    feat: chunk upload

--- pkg/storage/local/local.go
+++ pkg/storage/local/local.go
@@ -1,28 +1,43 @@
 package local
 
 import (
-	"work-space/pkg/storage"
+	"fmt"
+	"github.com/GoCloudstorage/GoCloudstorage/pkg/storage"
+	"path"
+)
+
+var (
+	FileAlreadyExist = fmt.Errorf("file already exist")
 )
 
 type StorageEngine struct {
+	rootPath string
+	uploader chunkUploader
 }
 
-func (l *StorageEngine) Upload(request storage.UploadRequest) error {
+func (s *StorageEngine) UploadChunk(request storage.UploadChunkRequest) error {
+	dirPath := s.getFileDir(request.BucketName, request.FileMD5)
+	return s.uploader.saveChunk(dirPath, request.PartNum, request.Data)
+}
+
+func (s *StorageEngine) getFileDir(bucketName string, fileMD5 string) string {
+	return path.Join(s.rootPath, bucketName, fileMD5)
+}
+
+func (s *StorageEngine) GetTemporaryURL(fileMD5 string) string {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (l *StorageEngine) GetTemporaryURL(fileMD5 string) string {
+func (s *StorageEngine) GetPermanentURL(fileMD5 string) string {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (l *StorageEngine) GetPermanentURL(fileMD5 string) string {
-	//TODO implement me
-	panic("implement me")
+func (s *StorageEngine) Init(config storage.InitConfig) {
+	s.rootPath = path.Join(config.Endpoint, config.BucketName)
 }
 
-func (l *StorageEngine) Init(config storage.InitConfig) {
-	//TODO implement me
-	panic("implement me")
+func (s *StorageEngine) MergeChunk(fileMD5 string, partSize int, dataSize int) error {
+
 }
